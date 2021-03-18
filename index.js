80a94746d971e8b6464e5d52b0ff33ca0b63a4a7
Mierzwix <kamilmierzwa17@gmail.com>
2021-03-18 16:22:57 +0100

    zadanie 2

---
+++ index.js
@@ -0,0 +1,28 @@
+var users = [
+    { name: 'John', surname: 'Smith', age: 25, role: 'user' },
+    { name: 'Adam', surname: 'Johnson', age: 35, role: 'user' },
+    { name: 'Andy', surname: 'Cole', age: 18, role: 'user' },
+];
+var admins = [
+    { name: 'Matthew', surname: 'Ryan', age: 43, role: 'admin' },
+    { name: 'Adam', surname: 'Terry', age: 24, role: 'admin' },
+];
+function logPerson(person) {
+    console.log(person.name + " " + person.surname + ", " + person.age + ", " + person.role);
+    function filterPersons(persons, criteria) {
+        return persons.filter(criteria);
+    }
+    function namedAdam(param) {
+        return param.name === 'Adam';
+    }
+    const persons = [...users, ...admins];
+    for (const p of persons)
+        logPerson(p);
+    console.log('---');
+    const ageAbove = persons.filter(p => p.age > 25);
+    for (const p of ageAbove)
+        logPerson(p);
+    console.log('---');
+    const filtered = filterPersons(persons, { name: 'Adam' });
+    for (const p of filtered)
+        console.log(p);
